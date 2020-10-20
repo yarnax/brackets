e1da86cbe385b85298a66570af4a643da50c4434
yarnax <feniks_63@mail.ru>
2020-10-20 12:38:06 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  console.log(str)
+  console.log(bracketsConfig)
+  let openBrackets = [];
+  let closeBrackets = [];
+  let summary = [];
+  for (a of bracketsConfig) {
+    openBrackets.push(a[0]);
+    closeBrackets.push(a[1]);
+  }
+  for (symbol of str) {   
+    if (closeBrackets.includes(symbol)) {
+      let openedBr = openBrackets[closeBrackets.indexOf(symbol)]
+      if (summary[summary.length-1] === openedBr) {
+        summary.splice(-1,1)
+      } else {
+        summary.push(symbol)
+        
+      }
+      //console.log(summary)
+      
+    } else if (openBrackets.includes(symbol)) {
+        summary.push(symbol); 
+    }   
+
+  }
+  console.log(summary);
+  return (summary.length === 0);
 }
